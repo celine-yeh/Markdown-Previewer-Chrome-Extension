20573db15717998c2d31b571a1836d3af6e92156
Celine Yeh <celineyeh@kkbox.com>
2020-07-10 12:37:42 +0800

    Preview markdown when fullscreen

--- markdown_previewer.js
+++ markdown_previewer.js
@@ -1,6 +1,15 @@
+var md = window.markdownit()
+
 $('.toolbar-fullscreen-btn').click(function (e) {
-  $('.js-md-preview-button').click()
-  $('.md-preview-holder').addClass('preview-fullscreen')
-  $('.md-write-holder').addClass('show')
-  $('zen-backdrop div-dropzone-wrapper').addClass('fullscreen')
+  $('.md-write-holder').append("<div id='markdown-previewer'></div>")
+
+  $('#markdown-previewer').html(md.render($('#note-body').val()))
+})
+
+$('#note-body').on('input', function (e) {
+  $('#markdown-previewer').html(md.render(this.value))
+})
+
+$('.zen-control-leave').click(function (e) {
+  $('#markdown-previewer').remove()
 })
